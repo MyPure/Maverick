69e7d65d7d445ea439b83f1e07ce831b8c1bd7a0
MyPure <cyn761480091@qq.com>
2019-05-29 20:55:07 +0800

    添加平台

---
+++ Scripts/Platform.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    float existTime;//存在时间
+    public static float startDropTime = 3.0f;//开始掉落时间
+    public float dropInterval = 0.5f;//掉落间隔
+    public int order;//掉落次序，从0开始
+    void Start()
+    {
+        existTime = 0;
+    }
+    void Update()
+    {
+        existTime += Time.deltaTime;
+        if (existTime - startDropTime > dropInterval * order)
+        {
+            drop();
+        }
+        if (transform.position.y < -6)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    float velocity = 0;
+    void drop()
+    {
+        GetComponent<Collider2D>().enabled = false;
+        velocity += 10.0f * Time.deltaTime;
+        transform.Translate(Vector3.down * velocity * Time.deltaTime);
+    }
+}
