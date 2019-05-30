a387691e408c24a6c9b9f1b4b75a371dad553312
vertigooooo <tellmehowlow@gmail.com>
2019-05-30 17:07:03 +0800

    瘴气和鬼火(预制体)

---
+++ Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 凡是具有“玩家接触即死”性质的敌人均应该挂载该脚本
+/// </summary>
+public class Enemy : MonoBehaviour
+{
+    //玩家对象
+    public GameObject player;
+
+    private void Start()
+    {
+        player = player ?? GameObject.FindGameObjectWithTag("Player");
+    }
+
+    //使用的是触发器，也可以使用碰撞器
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log(gameObject.name + "碰到玩家了");
+        }
+    }
+}
---
+++ Scripts/Miasma.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 瘴气
+/// </summary>
+public class Miasma : MonoBehaviour
+{
+    //玩家
+    public GameObject player;
+
+    //可见的距离
+    public float visibleDistance = 10;
+
+    //刚开始鬼火不显示
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        player = player ?? GameObject.FindGameObjectWithTag("Player");
+
+        gameObject.transform.position += new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, -0.01f);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
+    }
+
+    void Update()
+    {
+        //和玩家的距离小于4的时候可以显示
+        if (Mathf.Abs(gameObject.transform.position.x - player.transform.position.x) < visibleDistance)
+            spriteRenderer.enabled = true;
+        else
+            spriteRenderer.enabled = false;
+
+    }
+}
