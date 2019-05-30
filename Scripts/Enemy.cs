cb82fa948c41aa054b291d9584477eea46dae7a1
vertigooooo <tellmehowlow@gmail.com>
2019-05-30 17:45:22 +0800

    添加死亡后的处理、主页、按键函数类

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -10,6 +10,9 @@ public class Enemy : MonoBehaviour
     //玩家对象
     public GameObject player;
 
+    //死后的界面
+    public GameObject DeadUI;
+
     private void Start()
     {
         player = player ?? GameObject.FindGameObjectWithTag("Player");
@@ -21,6 +24,7 @@ public class Enemy : MonoBehaviour
         if(collision.tag == "Player")
         {
             Debug.Log(gameObject.name + "碰到玩家了");
+            Instantiate(DeadUI);
         }
     }
 }
