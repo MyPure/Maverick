b6e7424e179af65119e33799842bd5fb76e4b38b
MyPure <cyn761480091@qq.com>
2019-06-02 20:33:03 +0800

    碰撞改回trigger

--- Scripts/Enemies/Enemy.cs
+++ Scripts/Enemies/Enemy.cs
@@ -22,24 +22,14 @@ public class Enemy : MonoBehaviour
         //确保有碰撞盒
         if (gameObject.GetComponent<BoxCollider2D>() == null)
             gameObject.AddComponent<BoxCollider2D>();
-        //gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = true;
     }
 
-    ////使用的是触发器，也可以使用碰撞器
-    //private void OnTriggerEnter2D(Collider2D collision)
-    //{
-    //    if(collision.tag == "Player")
-    //    {
-    //        Debug.Log(gameObject.name + "碰到玩家了");
-    //        Instantiate(DeadUI);
-    //    }
-    //}
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "Player")
         {
             player.GetComponent<Player>().Die();
         }
-
     }
 }
