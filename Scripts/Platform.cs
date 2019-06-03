9e5843660d9ccd50b92e5d0e8f4aac84d9f9992a
MyPure <cyn761480091@qq.com>
2019-06-03 15:18:09 +0800

    增加Platforms的inspector扩展

--- Scripts/Platform.cs
+++ Scripts/Platform.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Platform : MonoBehaviour
+public class Platform : MonoBehaviour , IComparable
 {
     float existTime;//存在时间
     public static float startDropTime = 3.0f;//开始掉落时间
@@ -32,5 +33,11 @@ public class Platform : MonoBehaviour
         GetComponent<Collider2D>().enabled = false;
         velocity += 10.0f * Time.deltaTime;
         transform.Translate(Vector3.down * velocity * Time.deltaTime);
+    }
+
+    public int CompareTo(object obj)
+    {
+        float result = transform.position.x - ((Platform)obj).transform.position.x;
+        return result < 0 ? -1 : result > 0 ? 1 : 0;
     }
 }
---
+++ Scripts/Platforms.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Platforms : MonoBehaviour
+{
+
+}
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(Platforms))]
+public class PlatformsEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("SortPlatforms"))
+        {
+            List<Platform> platforms = new List<Platform>(((Platforms)target).GetComponentsInChildren<Platform>());
+            platforms.Sort();
+            for(int i = 0; i < platforms.Count; i++)
+            {
+                platforms[i].order = i;
+            }
+        }
+    }
+}
